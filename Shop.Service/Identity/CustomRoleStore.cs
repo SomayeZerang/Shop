2fc6ee0fef094988ce76e8dafa7157da88c8fe0c
SomayeZerang <somayezerang@gmail.com>
2018-09-08 10:37:06 +0430

    add Custome\userStore  and CustomeRoleStore class

---
+++ Shop.Service/Identity/CustomRoleStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Shop.Domain.Identity;
+
+namespace Shop.Service.Identity
+{
+    public class CustomRoleStore : RoleStore<Role, Guid, UserRoles>, ICustomRoleStore
+    {
+        public CustomRoleStore(DbContext context) : base(context)
+        {
+        }
+    }
+}
